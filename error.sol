421db58e5beaf7007d294bc0b9c8ba17930a7754
lakshsachdevv <109462107+lakshsachdevv@users.noreply.github.com>
2024-03-10 14:29:31 +0530

    Update error.sol

--- error.sol
+++ error.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+    contract FunctionsAndErrors {
+
+        uint public usdcPrice = 1;
+        uint public accountBalance;
+
+        function deposit(uint amount) public {
+            if(amount == 0) {
+                revert("CANNOT DEPOSIT 0 AMOUNT!!!");
+            }
+
+            accountBalance += amount;
+        } 
+
+        function withdraw(uint amount) public {
+            require(accountBalance >= amount, "INSUFFICIENT FUNDS");
+
+            accountBalance -= amount;
+        }
+
+        function checkStableCoin() public view returns (bool) {
+            assert(usdcPrice == 1);
+            return true;
+        }
+
+        function destabilizePrice(uint _usdcPrice) public {
+            usdcPrice += _usdcPrice;
+        }
+
+        function stabilizePrice() public {
+            usdcPrice -= usdcPrice;
+            usdcPrice += 1;
+        }
+    }
